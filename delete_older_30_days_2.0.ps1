5fb9d97b4018174f05cee04ecd2c73d8ac80fa6c
zloether <zloether@gmail.com>
2017-05-29 20:26:27 -0400

    Add files via upload

--- delete_older_30_days_2.0.ps1
+++ delete_older_30_days_2.0.ps1
@@ -1,4 +1,4 @@
-# delete_older_30_days_3.0.ps1
+# delete_older_30_days_2.0.ps1
 # deletes files older than 30 days
 
 Get-ChildItem -Recurse | Where-Object{$_.CreationTime -lt (Get-Date).AddDays(-30)} | Remove-Item -Recurse
